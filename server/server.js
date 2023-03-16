f5292383d626b481dcd613a09e203aa2021a5a7f
Gil Kagan <gilkag2@gmail.com>
2023-03-16 11:11:34 +0200

    connected server to python api

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
+import axios from 'axios';
 
 const app = express();
 
@@ -9,8 +10,9 @@ app.use(bodyParser.json());
 
 
 app.get('/', async (req, res) => {
-
+  const { data } = await axios.get('http://localhost:5000/test');
+  console.log(data);
   res.json('');
 });
 
-app.listen(3000, () => console.log("server is on"));
+app.listen(4000, () => console.log("server is on port 4000"));
