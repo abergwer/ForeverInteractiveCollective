6d89e0fcf575a65433a8f6b47e05a564b9a55fcf
Or Gerasi <orgerasi@gmail.com>
2023-03-16 18:32:07 +0200

    colorize photo class + supporting route in api to colorize photo

--- api.py
+++ api.py
@@ -1,3 +1,6 @@
+import base64
+from io import BytesIO
+from PIL import Image
 from asyncio.windows_events import NULL
 from dis import dis
 from turtle import distance
@@ -6,23 +9,39 @@ flask.helpers._endpoint_from_view_func = flask.scaffold._endpoint_from_view_func
 from flask import Flask,request,Response,json
 from flask_cors import CORS
 from flask_restful import Resource, Api
-
+from colrizeImage import ColorizeImage
+import os
 app = Flask(__name__)
 CORS(app)
 api = Api(app)
 
+colorizer = ColorizeImage()
 
-
-class Test(Resource):
-    def get(self):
+class Colorize(Resource):
+    def post(self):
         try:
-            print("hiii")
-            return Response(response="succses",status=200)
-        except:
-            return Response(response="failed",status=400)
+            # Decode the base64-encoded JPEG and convert it to a PIL Image object
+            encoded_jpeg = request.form['image']
+            decoded_jpeg = base64.b64decode(encoded_jpeg)
+            image = Image.open(BytesIO(decoded_jpeg))
+            image.save(r"C:\Users\afeka\Desktop\New folder\colorization\imgs\img_to_colorize.jpg")
+            colorizer.colorize_picture()
+            colorrized_pic = Image.open(r"C:\Users\afeka\Desktop\New folder\colorization\imgs_out\colorized_siggraph17.png")
+            #decoded_jpeg = base64.b64decode(colorrized_pic)
+            buffer = BytesIO()
+            colorrized_pic.save(buffer,format='png')
+            encoded_processed_jpeg = base64.b64encode(buffer.getvalue()).decode('utf-8')
+
+           # os.remove(r"C:\Users\afeka\Desktop\New folder\colorization\imgs\img_to_colorize")
+
+            
+
+            return Response(response=encoded_processed_jpeg,status=200)
+        except Exception as e:
+            return Response(response=f"failed,{e}",status=400)
 
 
-api.add_resource(Test,'/test/')
+api.add_resource(Colorize,'/ColorizePhoto')
 
 
 if __name__ == '__main__':
