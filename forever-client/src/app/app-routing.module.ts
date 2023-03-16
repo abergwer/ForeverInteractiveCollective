6c053fdfc8a5f69fb5e5d728356b9fac234b2eff
ashi <ashi.berg@gmail.com>
2023-03-16 16:36:13 +0200

    added the router of the login

--- forever-client/src/app/app-routing.module.ts
+++ forever-client/src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from '../login/login.component';
+import { SignInComponent } from '../signUpandIn/sign-in/sign-in.component';
+import { LoginComponent } from '../signUpandIn/login/login.component';
 
 const routes: Routes = [
-  {path : '', component : LoginComponent}
+  {path : '', component : LoginComponent},
+  {path : '/signIn', component : SignInComponent},
+  {path: '/signUp', component : LoginComponent}
 ];
 
 @NgModule({
