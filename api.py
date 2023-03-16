1ff242d8fb511fcc5dfe5d74bd762469869ec501
Or Gerasi <orgerasi@gmail.com>
2023-03-16 21:22:21 +0200

    added shitty photo sharpness

--- api.py
+++ api.py
@@ -1,6 +1,6 @@
 import base64
 from io import BytesIO
-from PIL import Image
+from PIL import Image,ImageFilter
 from asyncio.windows_events import NULL
 from dis import dis
 from turtle import distance
@@ -10,8 +10,14 @@ from flask import Flask,request,Response,json
 from flask_cors import CORS
 from flask_restful import Resource, Api
 from colrizeImage import ColorizeImage
+import sys
 import os
+import cv2
+import numpy as np
+
+
 app = Flask(__name__)
+app.config['MAX_CONTENT_LENGTH'] = sys.maxsize
 CORS(app)
 api = Api(app)
 
@@ -21,15 +27,32 @@ class Colorize(Resource):
     def post(self):
         try:
             # Decode the base64-encoded JPEG and convert it to a PIL Image object
-            encoded_jpeg = request.form['image']
+            #encoded_jpeg = request.form['image']
+            data = request.get_json()
+            encoded_jpeg = data['image']
             decoded_jpeg = base64.b64decode(encoded_jpeg)
             image = Image.open(BytesIO(decoded_jpeg))
-            image.save(r"C:\Users\afeka\Desktop\New folder\colorization\imgs\img_to_colorize.jpg")
+            sharpened_image = image.filter(ImageFilter.SHARPEN)
+            sharpened_image.save(r"C:\Users\afeka\Desktop\New folder\colorization\imgs\img_to_colorize.jpg")
+            
+            #sharpened_image = Image.fromarray(upsampled_array)
+
+            #sharpened_image.save(r"C:\Users\afeka\Desktop\New folder\colorization\imgs\img_to_colorize.jpg")
+
+            #img = cv2.imread(r'C:\Users\afeka\Desktop\New folder\colorization\imgs\img_to_colorize.jpg')
+
+            # Apply a sharpening filter
+            #kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
+            #sharpened = cv2.filter2D(img, -1, kernel)
+
+            # Save the sharpened image
+            #cv2.imwrite(r'C:\Users\afeka\Desktop\New folder\colorization\imgs\img_to_colorize.jpg', sharpened)
             colorizer.colorize_picture()
             colorrized_pic = Image.open(r"C:\Users\afeka\Desktop\New folder\colorization\imgs_out\colorized_siggraph17.png")
+            sharpened_image = colorrized_pic.filter(ImageFilter.SHARPEN)
             #decoded_jpeg = base64.b64decode(colorrized_pic)
             buffer = BytesIO()
-            colorrized_pic.save(buffer,format='png')
+            sharpened_image.save(buffer,format='png')
             encoded_processed_jpeg = base64.b64encode(buffer.getvalue()).decode('utf-8')
 
            # os.remove(r"C:\Users\afeka\Desktop\New folder\colorization\imgs\img_to_colorize")
